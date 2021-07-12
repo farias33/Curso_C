b4b6baa727fe67d6843dbd406e9f3a5e53c75088
Thales Fortes <thales.fortes21@gmail.com>
2021-07-12 10:08:11 -0300

    Add files via upload

---
+++ VariaveisInteiras.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main () {
+    // Definindo uma variavel
+    int a = 5, b;
+
+    // Imprimindo a variavel a
+    printf("%d\n", a);
+
+    // Concatenando
+    printf("O valor da var a e: %d\n", a);
+
+    // Lendo valores
+    scanf("%d", &b);
+
+    // Concatenando
+    printf("O valor da var b e: %d\n", b);
+
+    // Mudando o valor de a
+    a = 15;
+    printf("O novo valor da var a e: %d\n", a);
+
+
+
+
+    // Pausando
+    system("pause");
+
+
+}
